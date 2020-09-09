caddef6bda33b93fc43a4c78f94a4d9bd7083886
Ignacio Carvajal Duran <ignaciocarvajad@gmail.com>
2020-09-08 21:47:09 -0300

    add properties swagger

--- src/games/dto/games.dto.ts
+++ src/games/dto/games.dto.ts
@@ -2,8 +2,12 @@ import { ApiProperty } from '@nestjs/swagger';
 export class CreateGamesDTO {
   @ApiProperty()
   readonly title: string;
+  @ApiProperty()
   readonly description: string;
+  @ApiProperty()
   readonly price: number;
+  @ApiProperty()
   readonly imageUrl: string;
+  @ApiProperty()
   readonly category: string;
 }
